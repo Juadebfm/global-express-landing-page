bbe8e4ff6e89c3e680695d816ba64ed04803e9a1
Julius Adebowale <juadebgabriel@gmail.com>
2026-03-08 01:36:14 +0100

    feat: fix

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { DASHBOARD_URL } from '../constants/siteData';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
